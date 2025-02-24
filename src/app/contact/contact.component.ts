2018e8342c7e8ea91d96603b1aef6b6034b91335
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-02-24 06:47:07 +0100

    added contact and fixed bugs

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+
+  accept:boolean = false;
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  changeStatusPolicy() {
+    if(this.accept) {
+      this.accept = false;
+    } else if (!this.accept) {
+      this.accept = true;
+    }
+  }
+
+  onSubmit() {
+    console.log(this.contactData)
+  }
 
 }
