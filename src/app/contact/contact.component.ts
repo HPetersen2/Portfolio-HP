b92dc4721d4eb70d095b92ad16bfcfe49f2d7987
Henrik Petersen <hpet@deop0509.autoconnect.de>
2025-02-27 15:45:00 +0100

    fixed contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgClass } from '@angular/common';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, TranslateModule, NgClass],
+  imports: [FormsModule, TranslateModule, NgClass, NgStyle],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
   accept:boolean = false;
+  touchedAccept:boolean = false;
 
   contactData = {
     name: "",
@@ -25,13 +27,16 @@ export class ContactComponent {
       this.accept = false;
     } else if (!this.accept) {
       this.accept = true;
+      this.touchedAccept = true;
     }
   }
 
   onSubmit(ngForm: NgForm) {
     if(ngForm.valid && ngForm.submitted && this.accept) {
       console.log(this.contactData);
-
+      this.accept = false;
+      this.touchedAccept = false;
+      ngForm.reset();
     }
   }
 
