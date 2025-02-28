6ecfed9b9c24a3e65dbd1ace524ad1d7ca46a186
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-02-28 19:26:31 +0100

    added contact line left and add save language in localstorage

--- src/app/language.service.ts
+++ src/app/language.service.ts
@@ -10,8 +10,10 @@ export class LanguageService {
 
   constructor(private translate: TranslateService) {
     this.translate.addLangs(['de', 'en']);
+    let language:string = localStorage.getItem("currentLanguage") || "de";
+    this.currentLanguage = language;
     this.translate.setDefaultLang('de');
-    this.translate.use('de');
+    this.translate.use(language);
   }
 
   switchLanguage(lang:string):void {
@@ -19,11 +21,13 @@ export class LanguageService {
       case 'de': {
         this.currentLanguage = 'en';
         this.translate.use('en');
+        localStorage.setItem("currentLanguage", "en");
         break;
       }
       case 'en': {
         this.currentLanguage = 'de';
         this.translate.use('de');
+        localStorage.setItem("currentLanguage", "de");
         break;
       }
     }
