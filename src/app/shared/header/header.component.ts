92d3de0c8a9975492ea007944d02d757c8127e70
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-03-01 14:26:51 +0100

    added responsiv header and about me section

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
 import { LanguageService } from '../../language.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
   constructor(public languageService: LanguageService) {}
+
+  isBurgerMenuActive:boolean = false;
+
+  switchBurgerMenu() {
+    switch(this.isBurgerMenuActive) {
+      case false:
+        this.isBurgerMenuActive = true;
+        break;
+      case true:
+        this.isBurgerMenuActive = false;
+        break;
+      default:
+        this.isBurgerMenuActive = false;
+    }
+  }
 
 }
