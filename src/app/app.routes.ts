c6dc577521d9047a870793d8e571de301bb6eb96
Henrik Petersen <hpet@deop0509.autoconnect.de>
2025-03-07 09:54:15 +0100

    update route privacy-policy and imprint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'imprint', component: ImprintComponent},
+    { path: 'data-protection', component: DataProtectionComponent},
     { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
