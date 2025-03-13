cdf09de597ce9d844d1214fa357e87192d19022a
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-03-13 19:29:15 +0100

    add route child-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
 import { AboutMeComponent } from './landing-page/about-me/about-me.component';
-import { PortfolioComponent } from './landing-page/portfolio/portfolio.component';
 
 export const routes: Routes = [
-    { path: '', component: LandingPageComponent },
+    { path: '', component: LandingPageComponent, 
+        children: [
+            {
+                path: 'about-me',
+                component: AboutMeComponent
+            }
+        ]
+    },
     { path: 'imprint', component: ImprintComponent},
     { path: 'data-protection', component: DataProtectionComponent},
     { path: '**', redirectTo: '', pathMatch: 'full' },
